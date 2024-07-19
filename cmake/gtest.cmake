2007a7b6b82531663853f3871d8da88f4ee83aad
Qx <lqxlucky@qq.com>
2024-07-19 11:10:02 +0800

    fix gtest include path

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -3,7 +3,7 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-SET(GTEST_INCLUDE_DIR "${LIB_INCLUDE_DIR}/gtest" CACHE PATH "gtest include directory." FORCE)
+SET(GTEST_INCLUDE_DIR "${LIB_INCLUDE_DIR}" CACHE PATH "gtest include directory." FORCE)
 
 set(GTEST_LIBRARIES "${LIB_INSTALL_DIR}/libgtest.a" CACHE FILEPATH "gtest lib directory." FORCE)
 set(GTEST_MAIN_LIBRARIES "${LIB_INSTALL_DIR}/libgtest_main.a" CACHE FILEPATH "gtest main include directory." FORCE)
